ec987f42d229c818b1ee52ff7da14d8efebb2ea9
molinxo <jeanmolin16@gmail.com>
2026-08-08 23:51:59 -0300

    add logo 3

--- quartz/components/frames/DefaultFrame.tsx
+++ quartz/components/frames/DefaultFrame.tsx
@@ -25,7 +25,7 @@ export const DefaultFrame: PageFrame = {
       <>
         <div class="left sidebar">
           <img
-            src="jmcert/quartz/static/icon.png"
+            src="quartz/static/icon.png"
             alt="Logo"
             className="sidebar-logo"
             style={{ width: "150px", height: "auto" }} />
